ec5ece1212265b8900bcda91334c37147aef4736
Pobitrodeb <pobitrodn@gmail.com>
2023-03-13 21:57:35 +0600

    all done but not show maichimp email

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
+use Newsletter;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function subscribe(Request $request)
+    {
+        $request->validate([
+            'subscribe_email' => 'required|email',
+        ]);
+
+        try{
+
+            if(Newsletter::isSubscribed($request->subscribe_email)){
+                return redirect()->back()->with('error', 'This email already subscribed');
+            }else{
+
+                Newsletter::subscribe($request->subscribe_email);
+                return redirect()->back();
+            }
+        }catch(\Exception $e)
+        {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
+
+    }
+}
